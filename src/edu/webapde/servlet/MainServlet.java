ea5d492738bb28c9ef24b9f591e56627c82562d5
itsoverhere <courtneyngo@gmail.com>
2016-08-11 00:50:11 +0800

    first

---
+++ src/edu/webapde/servlet/MainServlet.java
@@ -0,0 +1,49 @@
+package edu.webapde.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.webapde.dto.Note;
+import edu.webapde.service.NotesService;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet(urlPatterns={"/MainServlet", "/main"})
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ArrayList<Note> notes = NotesService.getAllNotes();
+		request.setAttribute("notes", notes);
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+	}
+
+}
